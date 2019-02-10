24d23ff61d7e4f5f391ce60fe1318b38b7227fb5
Emirald Mateli <aldo.mateli@gmail.com>
2019-02-10 16:05:37 +0100

    read config from startupscript instead

--- utils/plasma.py
+++ utils/plasma.py
@@ -30,34 +30,26 @@ def safe_read_ini(filename):
     return config_parser
 
 
-def config_read(filename, group, key):
-    safe_read_ini(filename).get(group, key)
+def read_startup_script(filename):
+    return dict(safe_read_ini(filename)['__pytemp__'])
 
 
-def generate_startup_script(dst_filename, cursor_theme, screen_scale_factors,
-                            font_dpi, cursor_size, ksplash_engine,
-                            ksplash_theme):
+def generate_startup_script(dst_filename, **kwargs):
     contents_startup = """! /bin/sh
 
-    kcminputrc_mouse_cursortheme={cursor_theme}
-    kcminputrc_mouse_cursorsize={cursor_size}
-    ksplashrc_ksplash_theme={ksplash_theme}
-    ksplashrc_ksplash_engine={ksplash_engine}
-    kdeglobals_kscreen_screenscalefactors='{screen_scale_factors}'
-    kcmfonts_general_forcefontdpi={font_dpi}
-    """.format(cursor_theme=cursor_theme,
-               screen_scale_factors=screen_scale_factors,
-               font_dpi=font_dpi,
-               cursor_size=cursor_size,
-               ksplash_engine=ksplash_engine,
-               ksplash_theme=ksplash_theme)
+    kcminputrc_mouse_cursortheme={kcminputrc_mouse_cursortheme}
+    kcminputrc_mouse_cursorsize={kcminputrc_mouse_cursorsize}
+    ksplashrc_ksplash_theme={ksplashrc_ksplash_theme}
+    ksplashrc_ksplash_engine={ksplashrc_ksplash_engine}
+    kdeglobals_kscreen_screenscalefactors='{kdeglobals_kscreen_screenscalefactors}'
+    kcmfonts_general_forcefontdpi={kcmfonts_general_forcefontdpi}
+    """.format(**kwargs)
 
     open(dst_filename, 'w').write(contents_startup)
 
 
 def session_end():
-    subprocess.call(["qdbus", "org.kde.ksmserver",
-                    "/KSMServer", "logout", "0", "0", "0"])
+    subprocess.call(["qdbus", "org.kde.ksmserver", "/KSMServer", "logout", "0", "0", "0"])
 
 
 def kill():
