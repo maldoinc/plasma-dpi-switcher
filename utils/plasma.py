b5556397e0726b8c50b3cf4b473e81e13516f222
Emirald Mateli <aldo.mateli@gmail.com>
2019-02-23 22:47:11 +0100

    Added methods to read current profile settings

--- utils/plasma.py
+++ utils/plasma.py
@@ -11,6 +11,8 @@ CONFIG_STARTUP = os.path.expanduser('~/.config/startupconfig')
 CONFIG_SHELL = os.path.expanduser('~/.config/plasmashellrc')
 CONFIG_APPLETS = os.path.expanduser('~/.config/plasma-org.kde.plasma.desktop-appletsrc')
 
+SECTION_ROOT = 'root'
+
 
 def config_write(filename, group, key, value):
     params = ['kwriteconfig5', '--file', filename, '--key', key]
@@ -28,11 +30,28 @@ def config_write(filename, group, key, value):
     subprocess.call(params)
 
 
+def config_read(filename, group, key):
+    params = ['kreadconfig5', '--file', filename, '--key', key]
+
+    if isinstance(group, list):
+        for g in group:
+            params.append('--group')
+            params.append(str(g))
+    else:
+        params.append('--group')
+        params.append(str(group))
+
+    try:
+        return int(subprocess.check_output(params).splitlines()[0])
+    except ValueError:
+        return 0
+
+
 def safe_read_ini(filename):
     contents = open(filename, 'r').read()
 
     if not contents.startswith('['):
-        contents = '[__pytemp__]\n' + contents
+        contents = '[{}]\n{}'.format(SECTION_ROOT, contents)
 
     config_parser = configparser.RawConfigParser()
     config_parser.read_string(contents)
@@ -44,14 +63,14 @@ def session_end():
     subprocess.call(["qdbus", "org.kde.ksmserver", "/KSMServer", "logout", "0", "0", "0"])
 
 
-def kill():
+def shell_kill():
     subprocess.call(['kquitapp5', 'plasmashell'])
 
 
 def apply_profile(profile):
     scale_factors = display_get_scaling_str(output_list_names(), profile.scaling)
 
-    kill()
+    shell_kill()
 
     config_write(CONFIG_KDEGLOBALS, "KScreen", "ScaleFactor", profile.scaling)
     config_write(CONFIG_KDEGLOBALS, "KScreen", "ScreenScaleFactors", scale_factors)
@@ -66,3 +85,39 @@ def apply_profile(profile):
 
     for widget in profile.widgets:
         config_write(CONFIG_APPLETS, widget.groups, widget.key, widget.value)
+
+
+def plasmashell_find_panel_groups():
+    return[l.strip() for l in open(CONFIG_SHELL, 'r').readlines() if l.startswith('[PlasmaViews][Panel')]
+
+
+def sections_to_array(sections):
+    return sections[1:-1].split('][')
+
+
+def plasmashell_config_read_get_panel_info():
+    panels = plasmashell_find_panel_groups()
+    res = []
+
+    for p in panels:
+        groups = sections_to_array(p)
+
+        res.append({
+            "groups": groups,
+            "thickness": config_read(CONFIG_SHELL, groups, 'thickness')
+        })
+
+    return res
+
+
+def read_current_profile():
+    conf = safe_read_ini(CONFIG_STARTUP)
+
+    return {
+        "scaling": int(conf.get(SECTION_ROOT, 'kcmfonts_general_forcefontdpi')) / 96,
+        "cursor": {
+            "size": conf.get(SECTION_ROOT, 'kcminputrc_mouse_cursorsize')
+        },
+        "panels": plasmashell_config_read_get_panel_info(),
+        "widgets": {}
+    }
