52c9ca9a872811001ae2525f65dc6ea391d0d0fa
Emirald Mateli <aldo.mateli@gmail.com>
2019-03-15 23:44:02 +0100

    Generate a default config if it does not exist and no config is passed. Fixes #6

--- dpiswitchutils/utils.py
+++ dpiswitchutils/utils.py
@@ -69,3 +69,28 @@ def profile_remove(profile, filename):
 
 def save_config(c, filename):
     open(filename, 'w').write(json.dumps(c))
+
+
+def get_default_config():
+    return {
+        "version": "1.0",
+        "profiles": []
+    }
+
+
+def generate_default_config(fn):
+    os.makedirs(os.path.dirname(fn))
+    open(fn, 'w').write(json.dumps(get_default_config()))
+
+
+def get_default_config_filename_assert_exists(env):
+    """
+    Returns the default config filename and generates it with default
+    settings if it does not exist
+    """
+    fn = get_default_config_filename(env)
+
+    if not os.path.exists(fn):
+        generate_default_config(fn)
+
+    return fn
